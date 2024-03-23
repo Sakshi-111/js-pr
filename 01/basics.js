2b13699b9223f54509c9689460d9a1b5826df599
Sakshi-111 <142667224+Sakshi-111@users.noreply.github.com>
2024-03-23 07:39:31 +0000

    data conversion

--- 01/basics.js
+++ 01/basics.js
@@ -1,5 +1,26 @@
-const accName= "employee";
-let accPass= "tewo12";
-var accstart="themin";
-console.log(accName);
-console.table([accName,accPass,accstart])
+// const accName= "employee";
+// let accPass= "tewo12";
+// var accstart="themin";
+// console.log(accName);
+// console.table([accName,accPass,accstart])
+
+//  data conversion to number 
+// let num="33";
+// let num1= Number (num)
+// console.log (num1)
+// "33abs"=>NaN
+// "abs"=>NaN
+// TRUE=>1
+// FALSE =>0;
+
+// amswer is 33 typeof(num1)= >number
+// TO BOOLEAN  ANS IS TRUE/FALSE
+// 1=>true
+// 0=>false
+// ""=>false
+// "HASDH"=>true
+// console.log(Boolean("0"))
+// =>true because its a string
+// console.log(Boolean(0))===>false because number
+
+
