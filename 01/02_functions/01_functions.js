5a16be72f1dce3fda25f6a9549bf567265571c2b
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-04-27 12:04:55 +0530

    commit

---
+++ 01/02_functions/01_functions.js
@@ -0,0 +1,2 @@
+//whenever object is made using literals singleton object is  not made wheras whenever it is made using constructor singleton is maade 
+//object.create  is constructor method...// const jsuser={} no singleton
