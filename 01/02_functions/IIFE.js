25b8afa89fc6515c3d18d44731359454029134a6
Sakshi Aggarwal <142667224+Sakshi-111@users.noreply.github.com>
2024-05-23 18:43:01 +0000

    iife

---
+++ 01/02_functions/IIFE.js
@@ -0,0 +1,10 @@
+// IIFE IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
+// (function(){console.log("hellod")})()
+// WRITTEN TO SAVE FUNCTION FROM GLOABAL SCOPE POLLUTION
+// arrow function
+// (()=>{console.log(`hello`)})();
+
+
+// (function name(){
+//     console.log(`hekko`)
+// })();
