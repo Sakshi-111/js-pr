3720a38714cecc67232c298d7e3679be5135cfef
Sakshi Aggarwal <142667224+Sakshi-111@users.noreply.github.com>
2024-05-23 18:48:29 +0000

    comments

--- 01/02_functions/IIFE.js
+++ 01/02_functions/IIFE.js
@@ -1,10 +1,18 @@
-// IIFE IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
+// // IIFE IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
 // (function(){console.log("hellod")})()
-// WRITTEN TO SAVE FUNCTION FROM GLOABAL SCOPE POLLUTION
-// arrow function
-// (()=>{console.log(`hello`)})();
+// //WRITTEN TO SAVE FUNCTION FROM GLOABAL SCOPE POLLUTION
+// //arrow function
+// (()=>{
+//    // un named iife 
+//     console.log(`hello`)})();
 
 
 // (function name(){
+//     // named iife
 //     console.log(`hekko`)
 // })();
+
+// //passing parameter
+// ((parameter)=>{
+//    // un named iife 
+//     console.log(`hello`)})(arguement);
